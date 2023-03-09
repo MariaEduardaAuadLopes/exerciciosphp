3f6a3bcf2c83869e25e8ff2a5cc7764039993601
MariaEduardaAuadLopes <maria.ealopes2@senacsp.edu.br>
2023-03-08 22:04:23 -0300

    ok

---
+++ cadastro-cliente.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro Cliente</title>
+</head>
+<body>
+<form>
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<br>
+<select class="form-select" aria-label="Default select example">
+  <option selected></option>
+  <option value="1">SC</option>
+  <option value="2">RS</option>
+  <option value="3">PR</option>
+  <option value="4">SP</option>
+  <option value="5">RJ</option>
+  <option value="6">MG</option>
+  <option value="7">ES</option>
+  <option value="8">BA</option>
+  <option value="9">AC</option>
+  <option value="10">AL</option>
+  <option value="11">AP</option>
+  <option value="12">CE</option>
+  <option value="13">GO</option>
+  <option value="14">MA</option>
+  <option value="15">MT</option>
+
+</select>
